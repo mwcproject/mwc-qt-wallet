776fb6e34cad141ddf101a987dd7d7dc468a9fcb
bayk <k@k.com>
2019-10-06 11:45:17 -0700

    https://github.com/mwcproject/mwc-qt-wallet/issues/117 - Add resize back for main window. At main limit scale and apply it

--- main.cpp
+++ main.cpp
@@ -42,6 +42,9 @@
 #include "tests/testStringUtils.h"
 #include <QtGlobal>
 #include <QFileDialog>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QDesktopWidget>
 
 // Very first run - init everything
 bool deployFilesFromResources() {
@@ -166,6 +169,7 @@ int main(int argc, char *argv[])
 
     int retVal = 0;
 
+    while (true)
     {
 
         Q_ASSERT(argc>=1);
@@ -199,11 +203,48 @@ int main(int argc, char *argv[])
         if (scale>0.0)
             qputenv( "QT_SCALE_FACTOR", QString::number(scale).toLatin1() );
 
+    #else
+        scale = 1.0; // Mac OS, not applicable, mean 1.0
     #endif
 
         QApplication app(argc, argv);
         QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
         QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+
+        // Update scale if screen resolution is low...
+        // Unfortunatelly we can't do that before QApplication inited because Scree res API doesn't work
+        // That is why update now is pretty costly, we will need to testart the all because of that.
+        {
+            int minWidth = 10000;
+            int minHeight = 10000;
+
+            QList<QScreen *> screens = QGuiApplication::screens();
+            for ( QScreen * s : screens) {
+                QSize scrSz = s->availableSize() * scale;
+                minWidth = std::min( minWidth, scrSz.width() );
+                minHeight = std::min( minHeight, scrSz.height() );
+            }
+
+            double prevScale = scale;
+
+            if ( minWidth <= 1000 || minHeight <=600 )
+                scale = 1.0;
+            else if ( minWidth <= 1300 || minHeight <=700 )
+                scale = std::min(scale, 1.2);
+            else if ( minWidth <= 1500 || minHeight <=800 )
+                scale = std::min(scale, 1.4);
+            else if ( minWidth <= 1800 || minHeight <=950 )
+                scale = std::min(scale, 1.6);
+
+            if ( std::abs(scale-prevScale)>0.01 ) {
+                // need to update
+                appContext.setGuiScale(scale);
+                retVal = 1;
+                util::requestRestartMwcQtWallet();
+                break;
+            }
+        }
+
 
         logger::initLogger(appContext.isLogsEnabled());
 
@@ -308,6 +349,8 @@ int main(int argc, char *argv[])
         delete wndManager; wndManager=nullptr;
 
         util::releaseAppGlobalLock();
+
+        break;
     }
 
     // All objets are expected to be released at this point
