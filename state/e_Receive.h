c7b5d627f4824d2db3918d5c30c795e0ae10215e
kbay <kbay@vmware.com>
2020-03-02 21:16:06 -0800

    node status error at recieve. tru is fine because we recieve without a node

--- state/e_Receive.h
+++ state/e_Receive.h
@@ -55,7 +55,7 @@ public:
     virtual QString getResultTxPath() override {return "";}
     virtual void updateResultTxPath(QString path)  override {Q_UNUSED(path)}
 
-    virtual bool isNodeHealthy() const override {return false;}
+    virtual bool isNodeHealthy() const override {return true;}
 protected:
     virtual NextStateRespond execute() override;
     virtual QString getHelpDocName() override {return "receive.html";}
